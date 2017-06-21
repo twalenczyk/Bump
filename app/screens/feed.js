71d77a8b2edbae4a70c477ddb383a9f715c514e3
Taylor Walenczyk <twalen@Taylors-MacBook-Pro.local>
2017-06-21 11:14:47 -0500

    Added preleminary files

---
+++ app/screens/feed.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react'
+import {
+  Text,
+  View,
+  ScrollView
+} from 'react-native'
+import { List, ListItem } from 'react-native-elements'
+import { users } from '../config/data'
+
+class Feed extends Component {
+  render () {
+    <ScrollView>
+      <List>
+
+      </List>
+    </ScrollView>
+  }
+}
+
+export default Feed
