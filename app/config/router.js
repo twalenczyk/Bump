aeef62e2144301db02fe34c8fba2c28d7510bf8b
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-07-10 13:35:44 -0500

    Basic stacknav and tabnav setup

--- app/config/router.js
+++ app/config/router.js
@@ -3,14 +3,30 @@ import { TabNavigator, StackNavigator } from 'react-navigation'
 
 import Login from '../screens/login'
 import Feed from '../screens/feed'
+import Profile from '../screens/profile'
+
+const FeedTabs = TabNavigator({
+  Feed: {
+    screen: Feed,
+    navigationOptions: {
+      title: 'Feed'
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      title: 'Profile'
+    }
+  }
+})
 
 const LoginNavigator = StackNavigator({
   Login: {
     screen: Login,
   },
   Feed: {
-    screen: Feed,
-  },
+    screen: FeedTabs,
+  }
 })
 
 export default LoginNavigator
