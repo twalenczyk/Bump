0db4099715f49427018a8bad6be0bcc1076fa903
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-07-12 20:54:21 -0500

    Removed flex from app header and replaced with fixed height'

--- app/components/styles.js
+++ app/components/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
   header: {
-    flex: 1,
+    height: 35,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
