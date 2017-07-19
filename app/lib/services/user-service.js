e5824c7f25c997652e93f7463fbeb5e03a2f7e9d
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-07-19 15:33:42 -0500

    Framed up user-services

---
+++ app/lib/services/user-service.js
@@ -0,0 +1,23 @@
+/** Added by Taylor Walenczyk 7/19/17 **/
+
+export const SignUp = (userName, password, email, firstName, lastName) => {
+    // POST method
+    // Login not required
+    // Required params: userName password email firstName lastName
+    // NOTE email and userName should be globally unique. Validate before sending to backend.
+
+}
+
+export const GetUsers = () => {
+    // GET method
+    // Login required
+    // Required params: ids of users
+
+}
+
+export const UpdateProfile = () => {
+    // PUT method
+    // Login required
+    // Required params: none
+    // NOTE password rules apply and there are special instructions for pictures
+}
