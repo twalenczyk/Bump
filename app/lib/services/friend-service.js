ad9d0ebecd395af9cbe54dae2acf6b1e00db09a2
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-07-19 15:27:47 -0500

    Renamed user service to friend session service and changed methods within to reflect REST-ish API methods

---
+++ app/lib/services/friend-service.js
@@ -0,0 +1,26 @@
+/** Added by Taylor Walenczyk 7/19/17 **/
+
+/** Requests **/
+
+    export const CreateFriendRequest = (userId, friendId) => {
+        // POST method
+        // Login required
+        // Required params: userId friendId
+
+    }
+
+    export const AcceptFriendRequest = (userId, friendId) => {
+        // PUT method
+        // Login required
+        // Required params: userId friendId
+
+    }
+
+/** General **/
+
+    export const RemoveFriend = (userId, friendId) => {
+        // DELETE method
+        // Login required
+        // Required params: userId friendId
+
+    }
--- app/lib/services/user-service.js
+++
@@ -1,5 +0,0 @@
-/** Added by Taylor Walenczyk 7/19/17 **/
-
-export const login = () => {
-    
-}
