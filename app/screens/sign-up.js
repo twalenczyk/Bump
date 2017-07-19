12507795e5b9ebfabca75f1847a7c8d4206b60eb
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-07-19 14:08:52 -0500

    Basic linting, simple fleshed out sign up page, and empty services built

--- app/screens/sign-up.js
+++ app/screens/sign-up.js
@@ -1,22 +1,87 @@
 import React, { Component } from 'react'
 import {
-  Text,
-  View,
-  ScrollView
+    Button,
+    KeyboardAvoidingView,
+    Text,
+    TextInput,
+    View
 } from 'react-native'
 
 import styles from './styles'
 
 class SignUp extends Component {
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          This screen will be the sign up page!
-        </Text>
-      </View>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = {userName: '', password: '', email: '', firstName: '', lastName: ''}
+    }
+
+    render () {
+        return (
+            <KeyboardAvoidingView style={styles.loginContainer} behavior='padding'>
+                <View style={styles.loginBumpTextContainer}>
+                    <Text style={styles.loginBumpText}>
+                        Bump
+                    </Text>
+                </View>
+
+                <View style={styles.signupTextFieldContainer}>
+                    <TextInput
+                        style={styles.loginTextField}
+                        onChangeText={(text) => this.setState({userName: text})}
+                        value={this.state.userName}
+                        autoFocus={true}
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        placeholder='Name'
+                        />
+                    <TextInput
+                        style={styles.loginTextField}
+                        onChangeText={(text) => this.setState({password: text})}
+                        value={this.state.password}
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        secureTextEntry={true}
+                        placeholder='Password'
+                        />
+                    <TextInput
+                        style={styles.loginTextField}
+                        onChangeText={(text) => this.setState({email: text})}
+                        value={this.state.email}
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        placeholder='Email'
+                        />
+                    <TextInput
+                        style={styles.loginTextField}
+                        onChangeText={(text) => this.setState({firstName: text})}
+                        value={this.state.firstName}
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        placeholder='First Name'
+                        />
+                    <TextInput
+                        style={styles.loginTextField}
+                        onChangeText={(text) => this.setState({lastName: text})}
+                        value={this.state.lastName}
+                        autoCorrect={false}
+                        autoCapitalize='none'
+                        placeholder='Last Name'
+                        />
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <View style={styles.signupButton}>
+                        <Button
+                            style={{flex:1}}
+                            onPress={() => this.props.navigation.navigate('Sign Up')}
+                            title='Sign Up'
+                            color='white'
+                            />
+                    </View>
+              </View>
+            </KeyboardAvoidingView>
+        )
+    }
 }
 
 
