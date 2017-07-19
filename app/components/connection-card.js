564a228e567dda6b7b0dcfc136dc4b64399546d8
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-07-19 14:29:03 -0500

    Linting part 1

--- app/components/connection-card.js
+++ app/components/connection-card.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react'
 import {
-  Text,
-  View,
-  TouchableHighlight
+    Text,
+    View,
+    TouchableHighlight
 } from 'react-native'
 
 import styles from './styles'
 import ConnectionSubstance from './connection-substance'
 
 class ConnectionCard extends Component {
-  render () {
-    return (
-      <TouchableHighlight onPress={this.props.onPress} underlayColor="white">
-        <View style={styles.item}>
-          <ConnectionSubstance connection={this.props.connection} />
-        </View>
-      </TouchableHighlight>
-    )
-  }
+    render () {
+        return (
+            <TouchableHighlight onPress={this.props.onPress} underlayColor="white">
+                <View style={styles.item}>
+                    <ConnectionSubstance connection={this.props.connection} />
+                </View>
+            </TouchableHighlight>
+        )
+    }
 }
 
 
