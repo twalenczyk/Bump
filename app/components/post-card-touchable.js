67a80216100f49105dacdd3464757bcde466f9c3
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-07-22 11:00:43 -0500

    Patched the touchable highlight underlay color

--- app/components/post-card-touchable.js
+++ app/components/post-card-touchable.js
@@ -11,7 +11,7 @@ import PostCard from './post-card'
 class PostCardTouchable extends Component {
     render () {
         return (
-            <TouchableHighlight onPress={this.props.onPress} underlayColor='grey'>
+            <TouchableHighlight onPress={this.props.onPress} underlayColor='lightgrey'>
                 <View style={{flex: 1}}>
                     <PostCard post={this.props.post} profileNav={this.props.profileNav} />
                 </View>
